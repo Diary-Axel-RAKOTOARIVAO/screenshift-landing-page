96e94d5fe79f1a95786d0e526d9fc6e1309e1f7b
Dokja <revanamike620@gmail.com>
2025-06-06 17:58:43 +0300

    feat(toggle section): now better.

--- src/lib/sections/toggle.svelte
+++ src/lib/sections/toggle.svelte
@@ -7,8 +7,19 @@
 
 <section id="toggle">
 	<div class="content" data-screenshift={status}>
-		<h3>Wanna see something great ?</h3>
-		<h4>Click the button</h4>
-		<button onclick={debugMode} class="btn btn-primary">Debug Mode</button>
+		{#if !status}
+			<h3>Wanna see something great ?</h3>
+			<h4>Click the button</h4>
+		{:else}
+			<h3>Now try to resize your window</h3>
+			<h4>And see how the overlay tells you which breakpoints aliases to use</h4>
+		{/if}
+		<button onclick={debugMode} class="btn btn-primary">
+			{#if !status}
+				Debug Mode
+			{:else}
+				Normal Mode
+			{/if}
+		</button>
 	</div>
 </section>
