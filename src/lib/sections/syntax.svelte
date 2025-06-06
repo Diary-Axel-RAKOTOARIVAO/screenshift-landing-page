7d83bce3b557057d0de7013c643ea68f81c0f35d
Dokja <revanamike620@gmail.com>
2025-06-06 17:21:08 +0300

    feat(syntax section): looks nice not done yet tho

---
+++ src/lib/sections/syntax.svelte
@@ -0,0 +1,21 @@
+<script>
+	import html from '$lib/assets/images/syntax/html.webp';
+	import scss from '$lib/assets/images/syntax/scss.webp';
+</script>
+
+<section id="syntax">
+	<div class="content" data-screenshift>
+		<div class="title">
+			<h3>Introducing a new syntax</h3>
+			<h4>Cleaner code, less hastle</h4>
+		</div>
+		<div class="illustration">
+			<div class="html">
+				<img src={html} alt="" />
+			</div>
+			<div class="scss">
+				<img src={scss} alt="" />
+			</div>
+		</div>
+	</div>
+</section>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,7 @@
 <script>
 	import Hero from '$lib/sections/hero.svelte';
+	import Syntax from '$lib/sections/syntax.svelte';
 </script>
 
 <Hero />
+<Syntax />
