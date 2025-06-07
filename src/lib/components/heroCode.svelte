2bd5712e1f162d6aa36ea5e94173fd4063e9f8e1
Dokja <revanamike620@gmail.com>
2025-06-07 11:59:37 +0300

    feat(codeblock): works and look great ! Still need to add line numbers tho

--- src/lib/components/heroCode.svelte
+++ src/lib/components/heroCode.svelte
@@ -1,2 +1,37 @@
 <script>
+	import { Carta, Markdown } from 'carta-md';
+	import { code } from '@cartamd/plugin-code';
+	import DOMPurify from 'isomorphic-dompurify';
+	import '@cartamd/plugin-code/default.css';
+	const carta = new Carta({
+		sanitizer: DOMPurify.sanitize,
+		extensions: [
+			code({
+				theme: 'github-dark',
+				langs: ['scss']
+			})
+		],
+		shikiOptions: {
+			themes: ['github-dark']
+		}
+	});
+	let value = `
+\`\`\`css
+.testing {
+	background: red;
+	color: white;
+	padding: 10px;
+	border-radius: 5px;
+}
+`;
 </script>
+
+<div class="mockup-code">
+	<Markdown {carta} {value} />
+</div>
+
+<style>
+	:global(.shiki) {
+		background: none !important;
+	}
+</style>
